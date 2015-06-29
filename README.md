d613d94d842a3ce678b0cb031067ced56a28fb2c
Frederik Braun <fbraun+gh@mozilla.com>
2015-06-29 15:34:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ScanJS Rules through ESLint.
+
+If you want to get something like ScanJS using ESLint. This is the config 
+file.
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  globals: {
+    
+  },
+  rules: {
+    'no-unsafe-innerhtml': 2
+    'no-eval': 2,
+    'no-implied-eval': 2,
+    }
+};
